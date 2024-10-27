799433a2ef74329be6e3d51c10ae804b2c4516ce
KiritoKing <kiritoclzh@gmail.com>
2024-10-28 00:57:44 +0800

    chore: make navbar responsive

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -7,6 +7,75 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 import { LightDarkToggle } from "../ui/light-dark-toggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+type NavItem = {
+  title: string;
+  href: string;
+  icon?: React.ReactNode;
+};
+
+interface NavMenuProps {
+  items: NavItem[];
+  className?: string;
+}
+
+const NavMenu: React.FC<NavMenuProps> = ({ items, className }) => {
+  const renderMenu = (options?: { wrapWithRadix?: boolean }) => {
+    const { wrapWithRadix } = options ?? {};
+    return items.map((item) =>
+      wrapWithRadix ? (
+        <DropdownMenuItem key={item.href}>
+          <Link href={item.href}>{item.title}</Link>
+        </DropdownMenuItem>
+      ) : (
+        <li key={item.title} className="my-auto">
+          <Link href={item.href}>{item.title}</Link>
+        </li>
+      ),
+    );
+  };
+
+  return (
+    <>
+      <ul
+        className={cn(
+          "menu menu-horizontal hidden space-x-4 sm:flex",
+          className,
+        )}
+      >
+        {renderMenu()}
+      </ul>
+      {/* 移动端使用dropdown */}
+      <ul
+        className={cn(
+          "menu menu-horizontal flex space-x-4 sm:hidden",
+          className,
+        )}
+      >
+        <li className="my-auto">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Icon icon="material-symbols:menu" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {renderMenu({ wrapWithRadix: true })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </li>
+      </ul>
+    </>
+  );
+};
 
 interface Props {
   title?: string;
@@ -15,10 +84,21 @@ interface Props {
 const NavBar: React.FC<Props> = ({ title }) => {
   const pathname = usePathname();
 
+  const navItems: NavItem[] = [
+    {
+      title: "主页",
+      href: "/",
+    },
+    {
+      title: "博客",
+      href: "/blog",
+    },
+  ];
+
   return (
     <nav
       className={cn(
-        "navbar top-0 z-10 bg-opacity-30 px-20 backdrop-blur-lg backdrop-filter",
+        "navbar top-0 z-10 bg-opacity-30 px-8 backdrop-blur-lg backdrop-filter lg:px-20",
         pathname === "/" ? "fixed" : "sticky",
       )}
     >
@@ -27,14 +107,7 @@ const NavBar: React.FC<Props> = ({ title }) => {
           <Link href="/">{title}</Link>
         </h1>
         <div className="flex flex-none items-center">
-          <ul className="menu menu-horizontal">
-            <li>
-              <Link href="/">主页</Link>
-            </li>
-            <li>
-              <Link href="/blog">博客</Link>
-            </li>
-          </ul>
+          <NavMenu items={navItems} />
           <LightDarkToggle />
         </div>
       </div>
