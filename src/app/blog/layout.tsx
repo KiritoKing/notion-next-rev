99ddbd04ac27674acf4f506b42b483fd07ce5001
KiritoKing <kiritoclzh@gmail.com>
2024-10-30 01:09:13 +0800

    fix: style error

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
-    <div className="mx-auto my-10 max-w-[1200px] p-10 xl:px-0">{children}</div>
-  );
+  return <div className="mx-auto max-w-[1200px] p-10 xl:px-0">{children}</div>;
 };
 
 export default Layout;
